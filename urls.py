a82534571730f0d1c4178433d10def121d794341
SEVER <97824650+CepBep4@users.noreply.github.com>
2024-10-31 19:37:04 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,15 @@
+HOST = 'http://213.171.12.123:5000'
+
+urls = {
+    'user': f'{HOST}/api/user',
+    'promo': f'{HOST}/api/promo',
+    'case': f'{HOST}/api/case',
+    'history': f'{HOST}/api/history',
+}
+
+urls_admin = {
+    'user': f'{HOST}/admin/api/user',
+    'promo': f'{HOST}/admin/api/promo',
+    'case': f'{HOST}/admin/api/case',
+    'history': f'{HOST}/admin/api/history',
+}
