3dc126b0190dad7147e577c0316e31ffb2d554cf
SEVER <97824650+CepBep4@users.noreply.github.com>
2024-12-18 17:59:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from requests import post, get
 from dbcontrol import base_add
 
 bot = TeleBot(token='7465379572:AAFKE52yohq4jsoyNlyXQ-UELfH2oEnCJCI')
-HOST = 'https://cepbep4-airvapebot-3c83.twc1.net'
+HOST = 'https://cepbep4-airvapebot-b471.twc1.net'
 
 def register_new_user(uid,username):
     base_add('user',{
@@ -21,7 +21,7 @@ def get_user(uid):
 @bot.message_handler(commands=['start'])
 def listen(message):
     kb = types.InlineKeyboardMarkup()
-    kb.add(types.InlineKeyboardButton('Перейти', web_app=types.WebAppInfo('https://cepbep4-airvapebot-3c83.twc1.net')))
+    kb.add(types.InlineKeyboardButton('Перейти', web_app=types.WebAppInfo('https://cepbep4-airvapebot-b471.twc1.net')))
     bot.send_message(chat_id=message.chat.id, text='Добро пожаловать!', 
                     reply_markup=kb)
     register_new_user(message.chat.id, message.from_user.username)
